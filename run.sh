91ba29e43ea4bb9d750d3083f1b0b602f77bda44
shanejonas <jonas.shane@gmail.com>
2021-04-05 14:10:11 -0700

    Initial Commit

---
+++ run.sh
@@ -0,0 +1,16 @@
+export BRANCH_NAME="${GITHUB_REF##*}"
+export NEW_VERSION="${GITHUB_REF##*/${1}}"
+export RELEASE_BODY="$(awk -v version="${NEW_VERSION}" -f scripts/show-changelog.awk CHANGELOG.md)"
+
+node scripts/update-package-version.js
+
+if ! (git add . && git commit -m "Release ${NEW_VERSION}" && git push);
+then
+    echo "No changes"
+fi
+
+hub pull-request \
+    --draft \
+    --message "${NEW_VERSION} RC" --message "${RELEASE_BODY}" \
+    --base "main" \
+    --head "${BRANCH_NAME}";
