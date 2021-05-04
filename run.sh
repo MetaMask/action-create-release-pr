36e83cc65140b35534a9dc7c02849262e33006eb
Shane <jonas.shane@gmail.com>
2021-05-04 13:09:53 -0700

    fix: ordering of checkout + add bash validations

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
+#!/usr/bin/env bash
+
+set -x
+set -e
+set -u
+
 export NEW_VERSION="${1}"
 export BRANCH_NAME="release-${1}"
 export GITHUB_ACTION_PATH="${2}"
@@ -6,9 +12,9 @@ export RELEASE_BODY="$(awk -v version="${NEW_VERSION}" -f ${GITHUB_ACTION_PATH}/
 git config user.name github-actions
 git config user.email github-actions@github.com
 
-git checkout -b "${BRANCH_NAME}"
-
 node "${GITHUB_ACTION_PATH}/scripts/update-package-version.js"
+
+git checkout -b "${BRANCH_NAME}"
 
 if ! (git add . && git commit -m "Release ${NEW_VERSION}" && git push);
 then
