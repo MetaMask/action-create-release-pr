1dd83eadcd066de385c32d5da0a19be289cdb3f4
Shane <jonas.shane@gmail.com>
2021-05-04 13:03:02 -0700

    fix: checkout new branch before commit

--- run.sh
+++ run.sh
@@ -1,11 +1,14 @@
 export NEW_VERSION="${1}"
+export BRANCH_NAME="release-${1}"
 export GITHUB_ACTION_PATH="${2}"
 export RELEASE_BODY="$(awk -v version="${NEW_VERSION}" -f ${GITHUB_ACTION_PATH}/scripts/show-changelog.awk CHANGELOG.md)"
 
-node "${GITHUB_ACTION_PATH}/scripts/update-package-version.js"
-
 git config user.name github-actions
 git config user.email github-actions@github.com
+
+git checkout -b "${BRANCH_NAME}"
+
+node "${GITHUB_ACTION_PATH}/scripts/update-package-version.js"
 
 if ! (git add . && git commit -m "Release ${NEW_VERSION}" && git push);
 then
@@ -16,4 +19,4 @@ hub pull-request \
     --draft \
     --message "${NEW_VERSION} RC" --message "${RELEASE_BODY}" \
     --base "main" \
-    --head "${NEW_VERSION}";
+    --head "${BRANCH_NAME}";
