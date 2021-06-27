38d00d0879d1c0bd2cae7b4a22267b78ca4b3b86
Erik Marks <rekmarks@protonmail.com>
2021-06-27 14:15:25 -0700

    Add debug logging

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import { setFailed as setActionToFailed } from '@actions/core';
+import { error as logError, setFailed as setActionToFailed } from '@actions/core';
 import { performUpdate } from './update';
 import { getActionInputs } from './utils';
 
 performUpdate(getActionInputs()).catch((error) => {
+  logError(error?.stack || 'The error has no stack.');
   setActionToFailed(error);
 });
